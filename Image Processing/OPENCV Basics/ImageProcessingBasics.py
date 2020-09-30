c6e211ccbed74fe538a1a37cceb2830e987ad3ee
Rafael Santos <rafaelsantos.ee90@gmail.com>
2020-09-29 23:56:51 -0300

    Drawing on a image

--- Image Processing/OPENCV Basics/ImageProcessingBasics.py
+++ Image Processing/OPENCV Basics/ImageProcessingBasics.py
@@ -77,3 +77,29 @@ cv2.waitKey(0)
 blurred = cv2.GaussianBlur(image, (11, 11), 0)
 cv2.imshow("Blurred", blurred)
 cv2.waitKey(0)
+
+# draw a 2px thick red rectangle surrounding the face
+output = image.copy()
+cv2.rectangle(output, (320, 60), (420, 160), (0, 0, 255), 2)
+cv2.imshow("Rectangle", output)
+cv2.waitKey(0)
+
+# draw a blue 20px (filled in) circle on the image centered at
+# x=300,y=150
+output = image.copy()
+cv2.circle(output, (300, 150), 20, (255, 0, 0), -1)
+cv2.imshow("Circle", output)
+cv2.waitKey(0)
+
+# draw a 5px thick red line from x=60,y=20 to x=400,y=200
+output = image.copy()
+cv2.line(output, (60, 20), (400, 200), (0, 0, 255), 5)
+cv2.imshow("Line", output)
+cv2.waitKey(0)
+
+# draw green text on the image
+output = image.copy()
+cv2.putText(output, "OpenCV + Jurassic Park!!!", (10, 25), 
+	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+cv2.imshow("Text", output)
+cv2.waitKey(0)
