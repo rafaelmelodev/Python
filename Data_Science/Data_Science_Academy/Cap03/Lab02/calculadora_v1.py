21cd0fd56cb121361c51defa7c389400c040fba9
Rafael Santos <rafaelsantos.ee90@gmail.com>
2021-07-06 11:26:44 -0300

    Python Fundamento Lab02

---
+++ Data_Science/Data_Science_Academy/Cap03/Lab02/calculadora_v1.py
@@ -0,0 +1,33 @@
+# Python Calculator
+
+import math
+import sys
+
+# Print the menu to the user
+print("******************PYTHON CALCULATOR*********************\n")
+print("Selecione o número da operação desejada:\n")
+print("1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão")
+
+# Request the desired menu option and the number to be operated
+operation = input("\nDigite a opção desejada (1/2/3/4):")
+firstNumber = float(input("\nDigite o primeiro número:"))
+secondNumber = float(input("Digite o segundo número:"))
+
+if operation == '1':
+    result = firstNumber + secondNumber
+    signal = '+'
+elif operation == '2':
+    result = firstNumber - secondNumber
+    signal = '-'
+elif operation == '3':
+    result = firstNumber * secondNumber
+    signal = '*'
+elif operation == 4:
+    result = firstNumber / secondNumber
+    signal = '/'
+else:
+    print("Operação inválida!\n")
+    sys.exit()
+    
+# Show the solution to the user
+print("O resultado da operação", firstNumber, signal, secondNumber,"=", result)
