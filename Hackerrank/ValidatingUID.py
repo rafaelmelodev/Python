2567e05db2a31f159fdfa10bd88b5a4ee5bbc569
Rafael Santos <rafaelsantos.ee90@gmail.com>
2021-11-09 15:04:07 +0100

    Validating UID

---
+++ Hackerrank/ValidatingUID.py
@@ -0,0 +1,39 @@
+# Validating Unique Identification Number (UID)
+
+# Number of inputs
+n = input() 
+
+i = 0
+
+while (i < int(n)):
+    # Receive new UID
+    UID = input()
+    # Capital letters
+    cap = 0
+    # Digits
+    dig = 0
+    Verification = "Invalid"
+    # Number of characters
+    NumberCharac = len(UID)
+    # Check if it is alpha numeric
+    alfaNumeric = UID.isalnum()
+    
+    # Number of upper letters
+    for c in UID:
+        if c.isupper():
+            cap += 1
+    # Number of digits
+    for d in UID:
+        if d in "0123456789":
+            dig += 1
+    
+    # Verifying the validity
+    if cap >= 2:
+        if dig >= 3:
+            if NumberCharac == 10:
+                if alfaNumeric:
+                    if len(set(UID)) == len(UID):
+                        Verification = "Valid"
+    
+    i += 1
+    print(Verification)
